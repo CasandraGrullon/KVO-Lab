9d80cf358451006bf32fde243d1c472d9a53c24c
Casandra <casandragrullon@pursuit.org>
2020-04-07 13:28:10 -0400

    finished tab ui

---
+++ KVO-Lab/MyAccountViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KVO-Lab
+//
+//  Created by casandra grullon on 4/7/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import UIKit
+
+class MyAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
